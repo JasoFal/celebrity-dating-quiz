cda76810c85406187bfe6f1f9da2a32999109bba
Jason Falk <jasonfalk9@gmail.com>
2022-12-13 13:30:25 -0800

    Add replaced remove class code with classList.remove(). Added error message functionality to it's own function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function hideResultsAndError() {
-  document.getElementById(`error-message`)
+  error();
   document.getElementById(`match`).setAttribute(`class`, `hidden`);
   document.getElementById(`dave`).setAttribute(`class`, `hidden`);
   document.getElementById(`funnyRock`).setAttribute(`class`, `hidden`);
@@ -8,7 +8,13 @@ function hideResultsAndError() {
   document.getElementById(`not-an-answer`).setAttribute(`class`, `hidden`);
 }
 
+function error() {
+  document.getElementById(`error-message`)
+}
 
+function hideFormInfo() {
+  document.getElementById(`hideUserInfo`).setAttribute(`class`, `hidden`);
+}
 
 window.onload = function () {
   document.querySelector(`form`).onsubmit = function (event) {
@@ -19,22 +25,23 @@ window.onload = function () {
     let favSeason = document.querySelector(`input#favoriteSeason`).value;
     if (favIceCream && favSeason) {
       if (favIceCream === `mint` && favSeason === `winter` || favIceCream === `cheesecake` && favSeason === `summer`) {
-        document.getElementById(`match`).removeAttribute(`class`);
-        document.getElementById(`dave`).removeAttribute(`class`);
+        document.getElementById(`match`).classList.remove(`hidden`);
+        document.getElementById(`dave`).classList.remove(`hidden`);
       } else if (favIceCream === `cheesecake` && favSeason === `spring` || favIceCream === `mint` && favSeason === `autumn`) {
-        document.getElementById(`match`).removeAttribute(`class`);
-        document.getElementById(`funnyRock`).removeAttribute(`class`);
+        document.getElementById(`match`).classList.remove(`hidden`);
+        document.getElementById(`funnyRock`).classList.remove(`hidden`);
       } else if (favIceCream === `mint` && favSeason === `spring` || favIceCream === `cheesecake` && favSeason === `autumn`) {
-        document.getElementById(`match`).removeAttribute(`class`);
-        document.getElementById(`crockWithAGlock`).removeAttribute(`class`);
+        document.getElementById(`match`).classList.remove(`hidden`);
+        document.getElementById(`crockWithAGlock`).classList.remove(`hidden`);
       } else if (favIceCream === `cheesecake` && favSeason === `winter` || favIceCream === `mint` && favSeason === `summer`) {
-        document.getElementById(`match`).removeAttribute(`class`);
-        document.getElementById(`sirReginald`).removeAttribute(`class`);
+        document.getElementById(`match`).classList.remove(`hidden`);
+        document.getElementById(`sirReginald`).classList.remove(`hidden`);
       } else {
-        document.getElementById(`not-an-answer`).removeAttribute(`class`);
+        document.getElementById(`not-an-answer`).classList.remove(`hidden`);
       }
     } else {
-      document.getElementById(`error-message`).removeAttribute(`class`);
+      document.getElementById(`error-message`).classList.remove(`hidden`);
     }
+    hideFormInfo();
     };
   };
