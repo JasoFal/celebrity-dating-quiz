498a501ce3d00d90dd8889f1c414f6c37f91d40c
Jason Falk <jasonfalk9@gmail.com>
2022-12-14 10:38:24 -0800

    Refactor code to remove ad after reset button has been clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,11 @@ function hideResultsAndError() {
   document.getElementById(`resetButton`).setAttribute(`class`, `hidden`);
 }
 
+function showResetBtnAndResult() {
+  document.getElementById(`resetButton`).classList.remove(`hidden`);
+  document.getElementById(`match`).classList.remove(`hidden`);
+}
+
 function error() {
   document.getElementById(`error-message`)
 }
@@ -17,44 +22,47 @@ function hideFormInfo() {
   document.getElementById(`hideUserInfo`).setAttribute(`class`, `hidden`);
 }
 
+function showFormInfo() {
+  document.getElementById(`hideUserInfo`).removeAttribute(`class`, `hidden`);
+}
+
 window.addEventListener(`load`, function () {
   let form = document.querySelector(`form`);
   form.addEventListener(`submit`, function (event) {
     event.preventDefault();
     hideResultsAndError();
-  
-  let favIceCream = document.querySelector(`input#favoriteIceCream`).value;
-  let favSeason = document.querySelector(`input#favoriteSeason`).value;
-  if (favIceCream && favSeason) {
-    if (favIceCream === `mint` && favSeason === `winter` || favIceCream === `cheesecake` && favSeason === `summer`) {
-      document.getElementById(`match`).classList.remove(`hidden`);
-      document.getElementById(`dave`).classList.remove(`hidden`);
-      document.getElementById(`resetButton`).classList.remove(`hidden`);
-    } else if (favIceCream === `cheesecake` && favSeason === `spring` || favIceCream === `mint` && favSeason === `autumn`) {
-      document.getElementById(`match`).classList.remove(`hidden`);
-      document.getElementById(`funnyRock`).classList.remove(`hidden`);
-      document.getElementById(`resetButton`).classList.remove(`hidden`);
-    } else if (favIceCream === `mint` && favSeason === `spring` || favIceCream === `cheesecake` && favSeason === `autumn`) {
-      document.getElementById(`match`).classList.remove(`hidden`);
-      document.getElementById(`crockWithAGlock`).classList.remove(`hidden`);
-      document.getElementById(`resetButton`).classList.remove(`hidden`);
-    } else if (favIceCream === `cheesecake` && favSeason === `winter` || favIceCream === `mint` && favSeason === `summer`) {
-      document.getElementById(`match`).classList.remove(`hidden`);
-      document.getElementById(`sirReginald`).classList.remove(`hidden`);
-      document.getElementById(`resetButton`).classList.remove(`hidden`);
+
+    let favIceCream = document.querySelector(`input#favoriteIceCream`).value;
+    let favSeason = document.querySelector(`input#favoriteSeason`).value;
+    if (favIceCream && favSeason) {
+      if (favIceCream === `mint` && favSeason === `winter` || favIceCream === `cheesecake` && favSeason === `summer`) {
+        document.getElementById(`dave`).classList.remove(`hidden`);
+      } else if (favIceCream === `cheesecake` && favSeason === `spring` || favIceCream === `mint` && favSeason === `autumn`) {
+        document.getElementById(`funnyRock`).classList.remove(`hidden`);
+      } else if (favIceCream === `mint` && favSeason === `spring` || favIceCream === `cheesecake` && favSeason === `autumn`) {
+        document.getElementById(`crockWithAGlock`).classList.remove(`hidden`);
+      } else if (favIceCream === `cheesecake` && favSeason === `winter` || favIceCream === `mint` && favSeason === `summer`) {
+        document.getElementById(`sirReginald`).classList.remove(`hidden`);
+      } else {
+        document.getElementById(`not-an-answer`).classList.remove(`hidden`);
+      }
     } else {
-      document.getElementById(`not-an-answer`).classList.remove(`hidden`);
-      document.getElementById(`resetButton`).classList.remove(`hidden`);
+      document.getElementById(`error-message`).classList.remove(`hidden`);
     }
-  } else {
-    document.getElementById(`error-message`).classList.remove(`hidden`);
-    document.getElementById(`resetButton`).classList.remove(`hidden`);
-  }
-  let reset = document.getElementById(`resetButton`)
-  reset.addEventListener(`submit`, function() {
-    hideResultsAndError();
-    hideFormInfo().classList.remove(`hidden`);
+    showResetBtnAndResult();
+    hideFormInfo();
   });
-  hideFormInfo();
+  let resetBtn = document.getElementById(`resetBtn`);
+  function advertAlert() {
+    window.alert(`this is an advertisement!`);
+  }
+  resetBtn.addEventListener(`click`, (event) => {
+    event.preventDefault();
+    if (event.target.value == `true`) {
+      event.target.value = false;
+      advertAlert();
+    }
+    hideResultsAndError();
+    showFormInfo();
   });
 });
